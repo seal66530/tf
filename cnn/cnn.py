5e06a4583efd90289199cebfd52a1534e86b8edd
miaowen <miaowen@sunmi.com>
2018-12-23 13:47:15 +0800

    1. add cnn.py

---
+++ cnn/cnn.py
@@ -0,0 +1,85 @@
+# -*- coding: utf-8 -*- 
+
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import numpy as np
+import argparse
+import sys
+ 
+def main(args):
+    image_raw_data = tf.gfile.FastGFile(args.image, 'r').read()
+    image_data_rgb = tf.image.decode_jpeg(image_raw_data)
+
+    #灰度图
+    image_data_gray = tf.image.rgb_to_grayscale(image_data_rgb)
+
+    #max pool
+    image_data_max_pool = tf.nn.max_pool(tf.expand_dims(image_data_rgb, 0),  #扩展维度到4
+                                         ksize=[1,2,2,1], strides=[1,2,2,1], padding='SAME')
+
+    #image_data_max_pool = tf.to_float(image_data_max_pool)
+
+    #avg pool
+    image_data_avg_pool = tf.nn.avg_pool(tf.to_float(tf.expand_dims(image_data_rgb, 0)),  #扩展维度到4
+                                         ksize=[1,2,2,1], strides=[1,2,2,1], padding='SAME')
+
+    with tf.Session() as sess:
+        if args.action == "gray":
+            data_rgb_np, data_gray_np = sess.run([image_data_rgb, image_data_gray])
+            print data_rgb_np.shape
+            print data_gray_np.shape
+
+            plt.figure()
+            plt.subplot(1,2,1)
+            plt.imshow(data_rgb_np)
+
+            plt.subplot(1,2,2)
+            plt.imshow(data_gray_np[:,:,0], cmap ='gray')
+
+            plt.show()
+        elif args.action == "conv":
+
+            pass
+        elif args.action == "pool":
+            data_rgb_np, image_data_max_pool_np, image_data_avg_pool_np = sess.run([image_data_rgb, image_data_max_pool, image_data_avg_pool])
+
+            #print data_rgb_np.shape, image_data_max_pool_np.shape, image_data_avg_pool_np.shape
+            print image_data_max_pool_np.shape
+            #print image_data_max_pool_np
+
+            plt.figure()
+
+            plt.subplot(1,3,1)
+            plt.imshow(data_rgb_np)
+            np.savetxt("data_rgb_np[0].txt",data_rgb_np[:,:,0])
+            #np.savetxt("data_rgb_np[1].txt",data_rgb_np[:,:,1])
+            #np.savetxt("data_rgb_np[2].txt",data_rgb_np[:,:,2])
+            #print data_rgb_np
+
+            plt.subplot(1,3,2)
+            plt.imshow(image_data_max_pool_np[0,:,:,:])
+            #np.savetxt("image_data_max_pool_np[0].txt",image_data_max_pool_np[0,:,:,0])
+            #np.savetxt("image_data_max_pool_np[1].txt",image_data_max_pool_np[0,:,:,1])
+            #np.savetxt("image_data_max_pool_np[2].txt",image_data_max_pool_np[0,:,:,2])
+            #print image_data_max_pool_np
+
+            #plt.subplot(1,3,3)
+            #plt.imshow(image_data_avg_pool_np[0,:,:,:])
+
+            plt.show()
+        else:
+            pass
+
+def parse_arguments(argv):
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--image', type=str, 
+        help='input image path', default='./gyy.jpg')
+
+    parser.add_argument('--action', type=str, 
+        help='action: gray conv pool', default='pool')
+
+    return parser.parse_args(argv)
+
+if __name__ == '__main__':
+    main(parse_arguments(sys.argv[1:]))
