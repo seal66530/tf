1af31bea8076c8769fbcd81053e900e5ceb1cb32
miaowen <miaowen@sunmi.com>
2018-12-23 15:36:26 +0800

    1.cnn fix bug

--- cnn/cnn.py
+++ cnn/cnn.py
@@ -9,18 +9,23 @@ import sys
 def main(args):
     image_raw_data = tf.gfile.FastGFile(args.image, 'r').read()
     image_data_rgb = tf.image.decode_jpeg(image_raw_data)
+    #image_data_rgb = tf.image.convert_image_dtype(image_data_rgb, dtype=tf.float32)
+    #image_data_rgb = image_data_rgb * 255
 
     #灰度图
     image_data_gray = tf.image.rgb_to_grayscale(image_data_rgb)
 
+    #conv 2d
+    conv_filter = tf.constant([])
+    #image_data_conv = tf.nn.conv2d(tf.expand_dims(tf.to_float(image_data_rgb), 0),  #扩展维度到4
+    #                                     ksize=[1,2,2,1], strides=[1,2,2,1], padding='SAME')
+
     #max pool
-    image_data_max_pool = tf.nn.max_pool(tf.expand_dims(image_data_rgb, 0),  #扩展维度到4
+    image_data_max_pool = tf.nn.max_pool(tf.expand_dims(tf.to_float(image_data_rgb), 0),  #扩展维度到4
                                          ksize=[1,2,2,1], strides=[1,2,2,1], padding='SAME')
 
-    #image_data_max_pool = tf.to_float(image_data_max_pool)
-
     #avg pool
-    image_data_avg_pool = tf.nn.avg_pool(tf.to_float(tf.expand_dims(image_data_rgb, 0)),  #扩展维度到4
+    image_data_avg_pool = tf.nn.avg_pool(tf.expand_dims(tf.to_float(image_data_rgb), 0),  #扩展维度到4
                                          ksize=[1,2,2,1], strides=[1,2,2,1], padding='SAME')
 
     with tf.Session() as sess:
@@ -43,28 +48,23 @@ def main(args):
         elif args.action == "pool":
             data_rgb_np, image_data_max_pool_np, image_data_avg_pool_np = sess.run([image_data_rgb, image_data_max_pool, image_data_avg_pool])
 
-            #print data_rgb_np.shape, image_data_max_pool_np.shape, image_data_avg_pool_np.shape
-            print image_data_max_pool_np.shape
+            print data_rgb_np.shape,image_data_max_pool_np.shape,image_data_avg_pool_np.shape
             #print image_data_max_pool_np
 
             plt.figure()
 
             plt.subplot(1,3,1)
             plt.imshow(data_rgb_np)
-            np.savetxt("data_rgb_np[0].txt",data_rgb_np[:,:,0])
+            #np.savetxt("data_rgb_np[0].txt",data_rgb_np[:,:,0])
             #np.savetxt("data_rgb_np[1].txt",data_rgb_np[:,:,1])
             #np.savetxt("data_rgb_np[2].txt",data_rgb_np[:,:,2])
             #print data_rgb_np
 
             plt.subplot(1,3,2)
-            plt.imshow(image_data_max_pool_np[0,:,:,:])
-            #np.savetxt("image_data_max_pool_np[0].txt",image_data_max_pool_np[0,:,:,0])
-            #np.savetxt("image_data_max_pool_np[1].txt",image_data_max_pool_np[0,:,:,1])
-            #np.savetxt("image_data_max_pool_np[2].txt",image_data_max_pool_np[0,:,:,2])
-            #print image_data_max_pool_np
+            plt.imshow(image_data_max_pool_np[0,:,:,:].astype(np.uint8))
 
-            #plt.subplot(1,3,3)
-            #plt.imshow(image_data_avg_pool_np[0,:,:,:])
+            plt.subplot(1,3,3)
+            plt.imshow(image_data_avg_pool_np[0,:,:,:].astype(np.uint8))
 
             plt.show()
         else:
